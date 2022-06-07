419231d6eea17c421ae65b96ae97ea8dae902403
Umar Jabir Patel <ujp170030@sysbio.utdallas.edu>
2022-06-07 11:55:18 -0500

    Commit

--- align.sh
+++ align.sh
@@ -9,3 +9,9 @@ hisat2 -x $IDX -f -U refs/transcripts.fa | samtools sort > refs/transcripts.bam
 
 # Index the BAM file
 samtools index refs/transcripts.bam
+hisat2 -x refs/genome.fa -1 reads/BORED_1_R1.fq -2 reads/BORED_1_R2.fq | samtools sort > BORED_1.bam
+hisat2 -x refs/genome.fa -1 reads/BORED_2_R1.fq -2 reads/BORED_2_R2.fq | samtools sort > BORED_2.bam
+hisat2 -x refs/genome.fa -1 reads/BORED_3_R1.fq -2 reads/BORED_3_R2.fq | samtools sort > BORED_3.bam
+hisat2 -x refs/genome.fa -1 reads/EXCITED_1_R1.fq -2 reads/EXCITED_1_R2.fq | samtools sort > EXCITED_1.bam
+hisat2 -x refs/genome.fa -1 reads/EXCITED_2_R1.fq -2 reads/EXCITED_2_R2.fq | samtools sort > EXCITED_2.bam
+hisat2 -x refs/genome.fa -1 reads/EXCITED_3_R1.fq -2 reads/EXCITED_3_R2.fq | samtools sort > EXCITED_3.bam
